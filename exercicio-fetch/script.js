e63fa3966f7deb31ad8433a8b7be0d3a8e0738f6
guilacerdas <guilacerda@gmail.com>
2023-05-03 14:40:33 -0300

    Adiciona logica post usando jsonplaceholder

--- exercicio-fetch/script.js
+++ exercicio-fetch/script.js
@@ -5,31 +5,45 @@
 
     const response = await fetch("https://rickandmortyapi.com/api/character/");
     const json = await response.json();
-    console.log(json)
     const data = json.results
-    console.log(data)
-    const numberOfRequests = json.info.pages 
-    console.log(numberOfRequests)
-   
+
    
     if (json.results.length > 0) {
-      // for (let j = 2 ; j <= numberOfRequests; j++){
-      //    const urlRequest = `https://rickandmortyapi.com/api/character/?page=${j}`;
-      //    console.log(urlRequest);
-      //    const responses = fetch(`"${urlRequest}"`);
-      //    console.log(responses)
-      // }  
        postsArea.innerHTML = '';
        for (let i in data) {
           let postHtml = `<div style="display:flex;flex-direction:column;"><img src="${data[i].image}" alt="${data[i].name}" style="border-radius:100%;" ><div style="display:flex;"><h1>${data[i].name}</h1><div style="display:flex;gap:5%"><p>${data[i].species}:</p><p>${data[i].status}</p></div></div></div>`;
           postsArea.innerHTML += postHtml;
-       }
-       
+       }         
     } else {
        postsArea.innerHTML = 'nenhum Post Para Exibir';
     }
-    
- 
 }
 
+async function addNewPost(title, body) {
+   await fetch('https://jsonplaceholder.typicode.com/posts',
+      {
+         method: 'POST',
+         headers: {
+            'content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            title: title,
+            body: body,
+            userID: 2
+         })
+      }
+   );
+   document.querySelector('#titleField').value = '';
+   document.querySelector('#bodyField').value = '';
+   loadPosts();
+}
+document.querySelector("#insertButton").addEventListener('click', () => {
+   const title = document.querySelector("#titleField").value;
+   const body = document.querySelector("#bodyField").value;
+   if (title && body) {
+      addNewPost(title, body);
+   } else {
+      alert('preencha todos os campos')
+   }
+})
 loadPosts();
